3f40ec4871e68506b7ab7c105690900f933a48bc
Peter Watson <59647390+PJ-Watson@users.noreply.github.com>
2026-09-07 14:01:50 +0200

    Add model scaling factor.

--- src/bagpipes_extended/c_utils/__init__.py
+++ src/bagpipes_extended/c_utils/__init__.py
@@ -2,4 +2,4 @@
 Various functions written in C/Cython to improve performance.
 """
 
-from bagpipes_extended.c_utils.array_ops import calc_chisq
+from bagpipes_extended.c_utils.array_ops import calc_chisq, calc_scaling
--- src/bagpipes_extended/c_utils/array_ops.pyx
+++ src/bagpipes_extended/c_utils/array_ops.pyx
@@ -22,6 +22,7 @@ def calc_chisq(
     DTYPE_t[:,::1] models,
     DTYPE_t[::1] obs,
     DTYPE_t[::1] inv_sig_sq,
+    DTYPE_t[::1] scaling,
 ):
     """
     Calculate the chi-squared values for an array of models.
@@ -35,6 +36,9 @@ def calc_chisq(
     inv_sig_sq : 1D `~np.ndarray`
         A 1D array containing the inverse squared uncertainties for each
         of the M observations.
+    scaling : 1D `~np.ndarray`
+        A 1D array containing the scaling factors to be applied to each
+        of the N models.
 
     Returns
     -------
@@ -50,6 +54,55 @@ def calc_chisq(
 
     for i in range(I):
         for j in range(J):
-            chisq[i] += pow(models[i,j]-obs[j], 2)*inv_sig_sq[j]
+            chisq[i] += pow(models[i,j]*scaling[i]-obs[j], 2)*inv_sig_sq[j]
 
     return np.asarray(chisq)
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def calc_scaling(
+    DTYPE_t[:,::1] models,
+    DTYPE_t[::1] obs,
+    DTYPE_t[::1] inv_sig_sq,
+):
+    """
+    Calculate the scaling factor for an array of models.
+
+    Analogous to the default scaling in CIGALE, as bagpipes has no concept
+    of upper limits.
+
+    Parameters
+    ----------
+    models : 2D `~np.ndarray`
+        An array of shape (N, M), for N models, and M observations.
+    obs : 1D `~np.ndarray`
+        A 1D array containing M observations.
+    inv_sig_sq : 1D `~np.ndarray`
+        A 1D array containing the inverse squared uncertainties for each
+        of the M observations.
+
+    Returns
+    -------
+        scaling : 1D `~np.ndarray`
+        The scaling factor to match the models to observations, of shape
+        (N,).
+    """
+
+    I = models.shape[0]
+    J = models.shape[1]
+
+    cdef DTYPE_t[::1] num = np.zeros(I, dtype=DTYPE)
+    cdef DTYPE_t[::1] denom = np.zeros(I, dtype=DTYPE)
+
+    cdef DTYPE_t[::1] scaling = np.zeros(I, dtype=DTYPE)
+
+    for i in range(I):
+        for j in range(J):
+            num[i] += models[i,j] * (obs[j] * inv_sig_sq[j])
+            denom[i] += models[i,j] ** 2.0 * inv_sig_sq[j]
+
+    for i in range(I):
+        scaling[i] = num[i] / denom[i]
+
+    return scaling
